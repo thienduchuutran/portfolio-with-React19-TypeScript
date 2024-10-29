49ac6f7d583d63a6897931a42a3872df0681b4f0
Duc <thienductranhuu2784@gmail.com>
2024-10-29 19:20:51 -0400

    quick fix theme

--- src/components/context/app.context.tsx
+++ src/components/context/app.context.tsx
@@ -12,7 +12,7 @@ const AppContext = createContext<IAppContext | null>(null);
 
 export const AppContextProvider = ({children}: {children: React.ReactNode}) => {
     const [theme, setTheme] = useState<ThemeContextType>(() => {
-        const initialTheme = localStorage.getItem("theme") as ThemeContextType || "light";
+        const initialTheme = localStorage.getItem("theme") as ThemeContextType || "dark";
         return initialTheme;
     });
 
