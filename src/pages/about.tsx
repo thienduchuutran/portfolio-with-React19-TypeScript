2126710f95536b1f7fa77ac5b65963989ccc2404
Duc <thienductranhuu2784@gmail.com>
2026-04-23 18:40:03 -0400

    applied fading effects among all pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,15 @@
 import { Container } from "react-bootstrap";
 import About from "components/sections/about";
+import { useReveal } from "@/hooks/useReveal";
 
 const AboutPage = () => {
+    const aboutReveal = useReveal<HTMLElement>();
     return (
         <div className="about-screen">
-            <section className="mt-md-5 mt-2 pt-md-5 pt-0">
+            <section
+                ref={aboutReveal.ref}
+                className={`mt-md-5 mt-2 pt-md-5 pt-0 reveal ${aboutReveal.isVisible ? "is-visible" : ""}`}
+            >
                 <Container>
                     <About />
                 </Container>
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,10 +1,15 @@
 import { Container } from "react-bootstrap";
 import Project from "@/components/sections/project";
+import { useReveal } from "@/hooks/useReveal";
 
 const ProjectPage = () => {
+    const projectReveal = useReveal<HTMLElement>();
     return (
         <div className="project-screen">
-            <section className="mt-md-5 mt-2 pt-md-5 pt-0">
+            <section
+                ref={projectReveal.ref}
+                className={`mt-md-5 mt-2 pt-md-5 pt-0 reveal ${projectReveal.isVisible ? "is-visible" : ""}`}
+            >
                 <Container>
                     <Project />
                 </Container>
