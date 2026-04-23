49179921171885dbea66c66d089d3934347e8b34
Duc <thienductranhuu2784@gmail.com>
2026-04-23 19:02:18 -0400

    fixed reset scroll position on navigation

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { Outlet, useLocation } from "react-router-dom"
 import AppHeader from "./components/layout/app.header"
 import AppFooter from "./components/layout/app.footer"
 
 function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div>
