aec6a1466a6f6f09b7140cf275b47484537ed1ff
Duc <thienductranhuu2784@gmail.com>
2026-04-23 16:53:41 -0400

    upgraded resizebutton with new magnetic effect prop

--- src/components/sections/resize.button.tsx
+++ src/components/sections/resize.button.tsx
@@ -1,20 +1,31 @@
+import { useMagnetic } from "@/hooks/useMagnetic";
+
 interface IProps {
     btnText?: string;
     btnIcons?: React.ReactNode;
     btnStyle?: React.CSSProperties;
     onClick?: () => void;
-} 
+    magnetic?: boolean;
+}
 
 const ResizeButton = (props: IProps) => {
-    const { btnText, btnIcons, btnStyle, onClick } = props;
+    const { btnText, btnIcons, btnStyle, onClick, magnetic } = props;
+
+    const magneticRef = useMagnetic<HTMLSpanElement>();
 
     return (
-        <button 
-            onClick={onClick}
-            className="resize-button" style={btnStyle}>
-            <span style={{ textTransform: "uppercase" }}>{btnText}</span>
-            <>{btnIcons}</>
-        </button>
+        //span with inline-block so button's CSS keeps its own tranforms and the magnetic effect's transforms don't interfere with it
+        <span
+            ref={magnetic ? magneticRef : undefined}
+            style={{ display: "inline-block", willChange: magnetic ? "transform" : undefined }}
+        >
+            <button
+                onClick={onClick}
+                className="resize-button" style={btnStyle}>
+                <span style={{ textTransform: "uppercase" }}>{btnText}</span>
+                <>{btnIcons}</>
+            </button>
+        </span>
     )
 }
 
