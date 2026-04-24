726edec7b8be3413c5e08a7fc215ce1800515005
Duc <thienductranhuu2784@gmail.com>
2026-04-24 11:56:10 -0400

    show demo link for projects

--- src/components/sections/project/index.tsx
+++ src/components/sections/project/index.tsx
@@ -13,11 +13,11 @@ const Project = () => {
         <>
             <Row>
                 <Col xs={12}>
-                {currentLanguage == "en"? 
-                    <h3 className="text-center">{t("projects.header")} <span className="brand-red">{t("projects.work")}</span> </h3>
-                :
-                    <h3 className="text-center"><span className="brand-red">{t("projects.header")}</span> {t("projects.work")} </h3>
-                }
+                    {currentLanguage == "en" ?
+                        <h3 className="text-center">{t("projects.header")} <span className="brand-red">{t("projects.work")}</span> </h3>
+                        :
+                        <h3 className="text-center"><span className="brand-red">{t("projects.header")}</span> {t("projects.work")} </h3>
+                    }
                     <h6 className="text-center mb-md-5 mb-2">{t("projects.description")}</h6>
                 </Col>
             </Row>
@@ -32,7 +32,7 @@ const Project = () => {
                                 title={item.title[currentLanguage]}
                                 description={item.description[currentLanguage]}
                                 githubLink={item.githubLink}
-                                // demoLink={item.demoLink}
+                                demoLink={item.demoLink}
                             />
                         </Col>
                     )
